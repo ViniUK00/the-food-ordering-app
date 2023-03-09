246aeabf03b34e599fde04630bdc02c6771d11a8
ViniUK00 <ervin.bilo@yahoo.com>
2023-03-09 13:55:45 +0000

    added functionality to the modal to close and open on click

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -6,14 +6,14 @@ const Cart = props => {
         {id: 'c1', name: 'shushi', amount: 2, price: 12.99},
 ].map(item=><div>{item.name}</div>)}</ul>;
 
-    return <Modal>
+    return <Modal onClose={props.onClose}>
         {cartItems}
         <div className={classes.total}>
             <span>Total Amount</span>
             <span>35.62</span>
         </div>
         <div className={classes.actions}>
-            <button className={classes['button--alt']}>Close</button>
+            <button className={classes['button--alt']} onClick={props.onClose}>Close</button>
             <button className={classes.button}>Order</button>
         </div>
     </Modal>
--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -2,7 +2,7 @@ import classes from './HeaderCartButton.module.css';
 
 const HeaderCartButton = (props) =>{
     return (
-        <button className={classes.button}>
+        <button className={classes.button} onClick={props.onClick}>
             <span>Your Cart</span>
             <span className={classes.badge}> 3 </span>
         </button>
